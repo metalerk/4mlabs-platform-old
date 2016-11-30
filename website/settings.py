f50685362a5b6b1c0c581b2d57296116565a6ed5
Luis Esteban <metalerk@MBPdeLusEsteban.lan>
2016-11-30 00:23:43 -0600

    fix

--- website/settings.py
+++ website/settings.py
@@ -125,4 +125,4 @@ STATICFILES_DIR = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
