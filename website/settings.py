6205cbb5376ba8206b253e88c0e76ce1f2aab8a5
Luis Esteban <metalerk@MBPdeLusEsteban.lan>
2017-01-07 12:58:50 -0600

    grappelli added

--- website/settings.py
+++ website/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -35,6 +39,7 @@ ALLOWED_HOSTS += DEBUG_HOSTS
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -85,6 +90,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
--- website/urls.py
+++ website/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('home.urls')),
     url(r'^posts/', include('blog.urls', namespace='posts'))
